54c495ed57f652c2aa1c6084330257c8c454975e
Jim Read <jim.read@mail.com>
2021-03-12 00:45:27 +0000

    yml update

--- src/default_settings.py
+++ src/default_settings.py
@@ -11,7 +11,7 @@ class Config(object):
     def SQLALCHEMY_DATABASE_URI(self):                           
         value = os.environ.get("DB_URI")                            
         if not value:
-            raise ValueError("SQLALCHEMY_DATABASE_URI is not set")  
+            raise ValueError("DB_URI is not set")  
         return value  
 
 
